d797cacaa55c90b98badbec64d77354756aafd8c
agustinbarros13 <agustin-b_@outlook.es>
2024-06-12 14:13:26 +0200

    Corregir rutas de importación CSS y organización de archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Gallery from './components/Gallery'
-import '../Styles/App.css'
+import './Styles/App.css'
 const App = () => (
   <div className='App'>
     <h1>Pinterest-like Gallery</h1>
